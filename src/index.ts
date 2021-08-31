4051a1ec4662c2d1947184aee1ec2e6f3b5d6f32
Gilson Doi Junior <doijunior@yahoo.com.br>
2021-08-31 14:36:50 -0300

    state machine básica

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,71 @@
-import * as DB from 'nedb';
+import * as readline from 'readline-sync';
 
 
-interface Struct {
-  a: string;
-  id?: string;
+function soLiga(state: string){
+  console.log('ligou')
+  return 'LIG-PAR'
 }
 
-(async function(){
-  const db = new DB({ filename: './db', autoload: true });
+function ligaMov(state: string){
+  console.log('ligou e está em movimento')
+  return 'LIG-MOV'
+}
 
-  db.insert([{ a: 5 }, { a: 42 }, { a: 5 }, {b: 12, c: 'sdsdad'}], function (err) {
-    if(err) {
-      console.log(err)
-    } else {
-      console.log('saved')
-    }
-  });
-  
-  
-  db.find({}, function (err: any, docs: Struct[]) {
-    if(err) {
-      console.log(err)
-    } else {
-      console.log(docs)
-    }
-  });
-  
-  
-  await db.remove({a: 5}, { multi: false });
-  await db.update({a: 5}, { $set: {a: 10} });
+function soDesliga(state: string){
+  console.log('desligou')
+  return 'DES-PAR'
+}
 
-  const find: Promise<Struct[]> = new Promise((resolve, reject) => {
-    db.find({}, function(err: any, docs: Struct[]){
-      if(err){
-        reject(err)
-      }
-      resolve(docs)
-    })
-  });
-  
-  const list = (await find );
-  console.log(list);
-  
+function desligaMov(state: string){
+  console.log('desligou e está em movimento')
+  return 'DES-MOV'
+}
+
+function soMov(state: string){
+  console.log('começou a movimentar')
+  return 'DES-MOV'
+}
+
+function movLig(state: string){
+  console.log('começou a movimentar e está ligado')
+  return 'LIG-MOV'
+}
+
+function soPara(state: string){
+  console.log('parou')
+  return 'DES-PAR'
+}
+
+function paraLig(state: string){
+  console.log('parou e esta ligado')
+  return 'LIG-PAR'
+}
+
+
+(async function (){
+  let state: string = 'DES-PAR'
+  let transitionMatrix: any = {
+    'DES-PAR': {
+      '1': soLiga,
+      '2': soMov
+    },
+    'DES-MOV': {
+      '1': ligaMov,
+      '2': soPara 
+    },
+    'LIG-PAR': {
+      '1': soDesliga,
+      '2': movLig
+    },
+    'LIG-MOV': {
+      '1': desligaMov,
+      '2': paraLig
+    },
+  }
+  let input;
+  do {
+    input = readline.question('aperte a alternativa desejada: (1) botão 1 (2) botão 2 (s) Sair\n')
+
+    state = transitionMatrix[state][input]()
+  } while(input !== 's')
 })()
